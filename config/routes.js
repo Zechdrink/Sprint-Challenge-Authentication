2dd5def56f18193a5b7882eb484f4c769d382ee8
Zech <zechdrink@aol.com>
2019-04-05 10:41:56 -0700

    finished the self study questions

--- config/routes.js
+++ config/routes.js
@@ -40,7 +40,7 @@ function login(req, res) {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
 
-        res.status(200).json({ token });
+        res.status(200).json({ message: `here ya go ${user.username}`, token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -64,5 +64,19 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+// function generateToken(user){
+  
+//   const payload = {
+//     subject: user.id,
+//     username: user.username,
+//   }
+
+//   const options = {
+//     expiresIn: '1d',
+//   }
+
+//   return jwt.sign(payload, secret, options)
+// }
 
 
