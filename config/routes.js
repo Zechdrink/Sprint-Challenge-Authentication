9d8964658b5d33d2ac45fd49675cfbf868ad3a28
Zech <zechdrink@aol.com>
2019-04-05 08:40:13 -0700

    created the register endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const router = require('express').Router()
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -6,11 +9,25 @@ module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/', (req, res) => {
+    res.send("Is it working?");
+  });
 };
 
 function register(req, res) {
   // implement user registration
-}
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash; 
+
+    add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+} 
 
 function login(req, res) {
   // implement user login
@@ -30,3 +47,17 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+
